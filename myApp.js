1fc11653aab3ec947d1fdb799f569479e9ec4dd3
Ian Fullmer <ianfullmer@yahoo.com>
2021-06-22 11:02:35 -0700

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+//Use the .env file
 require('dotenv').config();
 
 
@@ -9,13 +10,17 @@ require('dotenv').config();
 
 
 
-
+// Meet the Node console
 console.log("Hello World");
 
 
 
-
-
+// Implement a Root-Level Request Logger Middleware
+app.use(function(req, res, next) {
+  console.log(req.method+" "+req.path+" - "+ req.ip);
+  next();
+}
+)
 
 
 
@@ -24,17 +29,13 @@ console.log("Hello World");
 
 app.use('/public', express.static(__dirname + '/public'));
 
-
+// Serve an HTML File
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 }
 );
 
-//   app.get("/json",function(req, res) {
-//     res.json({"message": "Hello json"} );
-//   }
-//   );
-
+// Serve JSON on a Specific Route
 app.get("/json", function (req, res) {
   (process.env.MESSAGE_STYLE === "uppercase" ? res.json({ "message": "HELLO JSON" }) : res.json({ "message": "Hello json" }))
 }
