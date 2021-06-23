e4e45077eeb0ed50c14d28e8bff1613aebe256fa
Ian Fullmer <ianfullmer@yahoo.com>
2021-06-23 13:18:22 -0700

    general clean up of white space and commented out superfluous code

--- myApp.js
+++ myApp.js
@@ -4,35 +4,26 @@ var app = express();
 require('dotenv').config();
 var bodyParser = require('body-parser');
 
-
-
-
-
-
-
 // Meet the Node console
 console.log("Hello World");
 
-
-
 // Implement a Root-Level Request Logger Middleware
-app.use(function(req, res, next) {
-  console.log(req.method+" "+req.path+" - "+ req.ip);
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
   next();
-}
-);
+});
+
 // Get Query Parameter Input from the Client
 app.get("/name", function (req, res) {
-  res.json({ name: req.query.first+" "+req.query.last});
-}
-);
+  res.json({ name: req.query.first + " " + req.query.last });
+});
+
 // Use body-parser to Parse POST Requests
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
 
 // Get Data from POST Requests
-app.post("/name", function(req, res) {
-  res.json({ name: req.body.first+" "+req.body.last})
+app.post("/name", function (req, res) {
+  res.json({ name: req.body.first + " " + req.body.last })
 });
 
 app.use('/public', express.static(__dirname + '/public'));
@@ -40,32 +31,25 @@ app.use('/public', express.static(__dirname + '/public'));
 // Serve an HTML File
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
-}
-);
+});
 
 // Chain Middleware to Create a Time Server
-app.get('/now', function(req, res, next) {
-  req.time = new Date().toString();  
+app.get('/now', function (req, res, next) {
+  req.time = new Date().toString();
   next();
-}, function(req, res) {
-  res.send({time: req.time});
+}, function (req, res) {
+  res.send({ time: req.time });
 });
 
 // Get Route Parameter Input from the Client
 app.get("/:word/echo", function (req, res) {
-  res.json({echo: req.params.word});
-}
-);
+  res.json({ echo: req.params.word });
+});
+
 // Serve JSON on a Specific Route
 app.get("/json", function (req, res) {
   (process.env.MESSAGE_STYLE === "uppercase" ? res.json({ "message": "HELLO JSON" }) : res.json({ "message": "Hello json" }))
-}
-);
-
-
-
-
-
+});
 
 
 
